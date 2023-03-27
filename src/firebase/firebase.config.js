ea762968f353cc344a6b125516c17246168761d4
ehsan-info <ehsanurrahman1992@gmail.com>
2023-03-27 17:08:12 +0300

    css edited

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAv8RpN-ykgCn8fnDIKbXhUBaZ-M7P7mxo",
+  authDomain: "inbank-84e8e.firebaseapp.com",
+  projectId: "inbank-84e8e",
+  storageBucket: "inbank-84e8e.appspot.com",
+  messagingSenderId: "62938169927",
+  appId: "1:62938169927:web:9c0efd9b06478bc562a762"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
